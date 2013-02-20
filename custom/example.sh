772f63d0d9b61124dd378cbb637e96427fbb431d
Ptah Dunbar <ptah@wpdevagency.com>
2013-02-20 18:33:30 +0100

    Updates for the day:
    + added: dotfiles support; installing, custom overriding and reverting works.
    + added: plugins support; installing, custom overriding works.
    + added: lol as a base plugin
    + added: example.sh as a base script
    + updated: yarrfiles installing, updating, initializations, uninstalling works (bash)

---
+++ custom/example.sh
@@ -0,0 +1,5 @@
+#
+# This is an example script that gets loaded on prompt login.
+#
+
+# echo "Yarr!"
--- yarrf.sh
+++ yarrf.sh
@@ -1,8 +1,75 @@
 #!/bin/sh
 
-echo "YARR!"
+# disable non-existing file matching
+shopt -s dotglob
+shopt -s nullglob
 
-echo "dotfiles"
-echo $dotfiles
-echo "plugins"
-echo $plugins
+# Get the current shell
+CSHELL=`basename $SHELL`
+
+# Set ZSH_CUSTOM to the path where your custom config files
+# and plugins exists, or else we will use the default custom/
+if [[ -z $YARRF_CUSTOM ]]; then
+    YARRF_CUSTOM="$YARRF/custom"
+fi
+
+is_plugin() {
+	local base_dir=$1
+	local name=$2
+
+	test -f $base_dir/plugins/$name/$name.plugin.$CSHELL \
+		|| test -f $base_dir/plugins/$name/$name.plugin.sh \
+		|| test -f $base_dir/plugins/$name/_$name
+}
+
+# autocompletion for zsh
+if [[ $CSHELL == 'zsh' ]]; then
+	for plugin in "${plugins[@]}"; do
+		if is_plugin $YARRF_CUSTOM $plugin; then
+			fpath=($YARRF_CUSTOM/plugins/$plugin $fpath)
+		elif is_plugin $YARRF $plugin; then
+			fpath=($YARRF/plugins/$plugin $fpath)
+		fi
+	done
+
+	autoload -U compinit
+	compinit -i
+fi
+
+# load all custom scripts
+for script in ${YARRF_CUSTOM}/*.{sh,${CSHELL}}; do
+	source $script
+done
+
+
+# Load all enabled plugins
+for plugin in "${plugins[@]}"; do
+	if [ -f "$YARRF_CUSTOM/plugins/$plugin/$plugin.plugin.$CSHELL" ]; then
+		source "$YARRF_CUSTOM/plugins/$plugin/$plugin.plugin.$CSHELL"
+	elif [ -f "$YARRF_CUSTOM/plugins/$plugin/$plugin.plugin.sh" ]; then
+		source "$YARRF_CUSTOM/plugins/$plugin/$plugin.plugin.sh"
+	elif [ -f "$YARRF_CUSTOM/plugins/$plugin/_$plugin" ]; then
+		source "$YARRF_CUSTOM/plugins/$plugin/_$plugin"
+	elif [ -f "$YARRF/plugins/$plugin/$plugin.plugin.$CSHELL" ]; then
+		source "$YARRF/plugins/$plugin/$plugin.plugin.$CSHELL"
+	elif [ -f "$YARRF/plugins/$plugin/$plugin.plugin.sh" ]; then
+		source "$YARRF/plugins/$plugin/$plugin.plugin.sh"
+	elif [ -f "$YARRF/plugins/$plugin/_$plugin" ]; then
+		source "$YARRF/plugins/$plugin/_$plugin"
+	else
+		echo "[yarrfiles] Plugin not found: ${plugin}"
+	fi
+done
+
+# Load the theme
+if [[ ! $YARRF_THEME = "" ]]; then
+	if [ -f "$YARRF_CUSTOM/$YARRF_THEME.$CSHELL-theme" ]; then
+		source "$YARRF_CUSTOM/$YARRF_THEME.$CSHELL-theme"
+	elif [ -f "$YARRF_CUSTOM/$YARRF_THEME.sh-theme" ]; then
+		source "$YARRF_CUSTOM/$YARRF_THEME.sh-theme"
+	elif [ -f "$YARRF/themes/$YARRF_THEME.$CSHELL-theme" ]; then
+		source "$YARRF/themes/$YARRF_THEME.$CSHELL-theme"
+	elif [ -f "$YARRF/themes/$YARRF_THEME.sh-theme" ]; then
+		source "$YARRF/themes/$YARRF_THEME.sh-theme"
+	fi
+fi
